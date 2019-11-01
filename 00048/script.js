10eaddd79e75572c69838121a676103eb62dc772
MohammadRezaAsghari <mrezaasghari.mra@gmail.com>
2019-11-02 00:01:23 +0330

    2_11_2019

--- 00048/script.js
+++ 00048/script.js
@@ -2,6 +2,23 @@ class Person{
     constructor(fName , lName , dob){
         this.fName = fName;
         this.lName = lName;
-        this.dob = dob;
+        this.dob = new Date(dob);
+        this.basicAgeCalc = function(){
+            console.log(2019 - this.dob.getFullYear());
+        }
+    }
+    greeting(){
+        return `Hi ${this.fName} ${this.lName}`
+    }
+    calculateAge(){
+        let timeStamp = Date.now();
+        let BirthTimeStamp = this.dob.getTime();
+        let diff = timeStamp - BirthTimeStamp;
+        let ageDate = new Date(diff);
+        console.log(ageDate.getFullYear() - 1970);
     }
 }
+
+let john = new Person('john' , 'Mead' , '8-1-1994');
+
+console.log(john.basicAgeCalc());
