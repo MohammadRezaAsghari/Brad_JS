504e2ac266194a143b2ca9a67ce522f0485c33ca
MohammadRezaAsghari <mrezaasghari.mra@gmail.com>
2019-12-02 23:11:31 +0330

    Added Preloader

--- 00060/script.js
+++ 00060/script.js
@@ -1,26 +1,39 @@
 document.getElementById('btnSubmit').addEventListener('click' , function(e) {
-
+    debugger;
     const number = document.getElementById('jokeNumber');
     const numberOfJokes = number.value;
+    debugger;
     number.value = '';
+    document.querySelector('.loader').style.display = 'block';
+    e.preventDefault();
     if(numberOfJokes > 0){
+        
         const xhr = new XMLHttpRequest();
-
+        
         xhr.open('GET' , `http://api.icndb.com/jokes/random/${numberOfJokes}` , true);
-
+        
         xhr.onload = function() {
+            
             const response = JSON.parse(this.responseText);
             const list = document.querySelector('.list-group');
+            list.addEventListener('load' , function() {
+                setTimeout(function () {
+                    document.querySelector('.loader').style.display = 'none';
+                } , 3000);
+                
+            })
             let output = '';
             response.value.forEach(function (item , index) {
             output +=`
             <li class='list-group-item'>${index+1} : ${item.joke}</li>
             `
+         
         });
             list.innerHTML = output;
     }
-
+   
     xhr.send();
+    
     }
     
     
