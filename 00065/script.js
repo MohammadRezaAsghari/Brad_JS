a466970019c6c91bdf1cefe890badbd14f275dbc
MohammadRezaAsghari <mrezaasghari.mra@gmail.com>
2019-12-07 17:33:48 +0330

    Js Promises

---
+++ 00065/script.js
@@ -0,0 +1,35 @@
+let postArray = [
+    {title:'First post' , body:'First post body'},
+    {title:'Second post' , body:'second post body'},
+    {title:'Third post' , body:'third post body'}
+]
+
+let createPost = (post , callback) =>{
+
+    return new Promise((resolve , reject) => {
+        setTimeout(()=> {
+            postArray.push(post);
+            resolve();
+        }, 2000);
+        
+    })
+
+   
+    
+}
+
+let getPost = () =>{
+    setTimeout(()=>{
+        let output = '';
+        postArray.forEach((item)=>{
+            output += `<li>${item.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    } , 1000);
+}
+
+createPost({title:'Forth Post' , body:'Forth Post Body'}).then(getPost);
+createPost({title : 'Fifth Post' , body:'Fifth post body'}).then(getPost);
+// getPost();
+
+console.log(postArray);
