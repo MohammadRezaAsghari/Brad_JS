fd2bda4b7c8f3819c04dc0c7fee6f6501a1972c0
MohammadRezaAsghari <mrezaasghari.mra@gmail.com>
2019-12-18 00:08:30 +0330

    12_18_2019

--- 00065/script.js
+++ 00065/script.js
@@ -1,35 +1,102 @@
-let postArray = [
-    {title:'First post' , body:'First post body'},
-    {title:'Second post' , body:'second post body'},
-    {title:'Third post' , body:'third post body'}
+
+let post = [
+    {
+        title : 'first title'
+    },{
+        title : 'second title'
+    }
 ]
 
-let createPost = (post , callback) =>{
-
-    return new Promise((resolve , reject) => {
-        setTimeout(()=> {
-            postArray.push(post);
+let createPost = (pst , callback) =>{
+    return new Promise((resolve , reject)=>{
+        setTimeout (()=>{
+            post.push(pst);
             resolve();
-        }, 2000);
-        
-    })
-
-   
+        } , 2000)
+    });
     
 }
 
 let getPost = () =>{
+    
     setTimeout(()=>{
-        let output = '';
-        postArray.forEach((item)=>{
-            output += `<li>${item.title}</li>`;
-        });
-        document.body.innerHTML = output;
-    } , 1000);
+        
+        console.log(post);
+    },1000)
 }
 
-createPost({title:'Forth Post' , body:'Forth Post Body'}).then(getPost);
-createPost({title : 'Fifth Post' , body:'Fifth post body'}).then(getPost);
+createPost({title:'thrid title'}).then(getPost);
+
 // getPost();
 
-console.log(postArray);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let postArray = [
+//     {title:'First post' , body:'First post body'},
+//     {title:'Second post' , body:'second post body'},
+//     {title:'Third post' , body:'third post body'}
+// ]
+
+// let createPost = (post , callback) =>{
+
+//     return new Promise((resolve , reject) => {
+//         setTimeout(()=> {
+//             postArray.push(post);
+//             resolve();
+//         }, 2000);
+        
+//     })
+
+   
+    
+// }
+
+// let getPost = () =>{
+//     setTimeout(()=>{
+//         let output = '';
+//         postArray.forEach((item)=>{
+//             output += `<li>${item.title}</li>`;
+//         });
+//         document.body.innerHTML = output;
+//     } , 1000);
+// }
+
+// createPost({title:'Forth Post' , body:'Forth Post Body'}).then(getPost);
+// createPost({title : 'Fifth Post' , body:'Fifth post body'}).then(getPost);
+// // getPost();
+
+// console.log(postArray);
